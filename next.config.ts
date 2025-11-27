a1890acf667910e520807ac1a202df87702a0ab2
sasaki-csngrp <sasaki@csngrp.co.jp>
2025-11-27 00:15:42 +0000

    fix: Vercelの制約に達したので、画像変換を停止

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // Vercelの画像変換を無効化
     remotePatterns: [
       {
         protocol: "https",
